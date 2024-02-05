a0c496ff4a33392caad50468fbd7c2b25c1e3cc5
Héctor Chocobar-Torrejón <45291267+hchocobar@users.noreply.github.com>
2024-02-05 20:41:11 +0000

    wip: main.jsx Routes.jsx actions.js

--- src/actions.js
+++ src/actions.js
@@ -3,21 +3,36 @@
 // It is the analogous to the "setter" function of the useState.
 
 export default ({ getStore, getActions, setStore }) => ({
-
-  exampleFunction: () => {
-    getActions().changeColor(0, 'green');
-  },
-
+  exampleFunction: () => { getActions().changeColor(0, 'green'); },
   changeColor: (index, color) => {
-    const store = getStore();
-    const demo = store.demo.map((elm, i) => {
-      if (i === index) elm.background = color;
-      return elm;
+    // const store = getStore();
+    const demo = getStore().demo.map((element, i) => {
+      if (i === index) {
+        element.background = color;
+      }
+      return element;
     });
-
-    //reset the global store
+    // Reset the global store
     console.log(demo)
     setStore({ demo: demo });
   }
-
 })
+
+
+/* 
+Syntax into actions:
+
+1. Usar getActions() para llamar a otra actions(function) dentro de una actions(fuction)
+	getActions().changeColor(0, "green")
+
+2. Utilizar getStore() para acceder al valor de un "store" dentro de un "actions"
+		2.1
+			const store = getStore();
+			store.demo.map()
+		2.2
+			getStore().demo.map()
+
+3. Utilizar setStore() para guardar un valor en un "store" (recordar que store es un objeto)
+	setStore({ demo: demo });
+
+*/
--- src/main.jsx
+++ src/main.jsx
@@ -1,33 +1,28 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import {  RouterProvider } from "react-router-dom";
 import { createContext } from 'react';
+import ReactDOM from 'react-dom/client'
+import {  RouterProvider } from "react-router-dom";
+import './index.css'
 // This is where all your routes are located
-import { router } from "./routes";
-import { useGlobalState } from "./hooks/useGlobalState";
+import { router } from "./Routes.jsx";
+import { useGlobalState } from "./hooks/useGlobalState.jsx";
+
 
 export const Context = createContext(null)
 
+
 const defaultGlobalState = {
   message: null,
-  demo: [
-    {
-      title: "FIRST",
-      background: "white",
-      initial: "white"
-    },
-    {
-      title: "SECOND",
-      background: "white",
-      initial: "white"
-    }
-  ]
+  demo: [{title: "FIRST", background: "white", initial: "white"},
+         {title: "SECOND", background: "white", initial: "white"}]
 }
+
 
 const Main = () => {
   const globalState = useGlobalState(defaultGlobalState)
-  if(window) window.globalState = globalState;
+  if (window) {
+    window.globalState = globalState;
+  }
   return (
     <React.StrictMode>  
       <Context.Provider value={globalState}>
@@ -36,5 +31,6 @@ const Main = () => {
     </React.StrictMode>
   );
 }
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(<Main />)
